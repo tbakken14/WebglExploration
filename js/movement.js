71d39f22b302ec5f923ee3f1cf1ee2d1487d835d
Thomas Bakken <tbakken14@gmail.com>
2023-10-03 21:26:52 -0700

    Update player movement

--- js/movement.js
+++ js/movement.js
@@ -4,27 +4,43 @@ class Movement {
             if (event.repeat) { return }
             switch (event.key) {
                 case "ArrowUp" :
-                case "W":
+                case "w":
                     playerModel.translationVelocity[0] += Math.cos(playerModel.rotation);
                     playerModel.translationVelocity[1] += Math.sin(playerModel.rotation);
                     break;
                 case "ArrowDown" :
-                case "S":
-                    playerModel.translationVelocity[0] -= Math.cos(playerModel.rotation);
-                    playerModel.translationVelocity[1] -= Math.sin(playerModel.rotation);
+                case "s":
+                    playerModel.translationVelocity[0] -= .5*Math.cos(playerModel.rotation);
+                    playerModel.translationVelocity[1] -= .5*Math.sin(playerModel.rotation);
                     break;
                 case "ArrowLeft" :
-                case "A":
-                    playerModel.rotationVelocity += .001;
+                case "a":
+                    playerModel.rotationVelocity = .03;
                     break;
                 case "ArrowRight" :
-                case "D":
-                    playerModel.rotationVelocity -= .001;
+                case "d":
+                    playerModel.rotationVelocity = -.03;
                     break;
                 default:
                     break;
             }
-        })
+        });
+        
+        document.addEventListener("keyup", (event) => {
+            if (event.repeat) { return }
+            switch (event.key) {
+                case "ArrowLeft" :
+                case "A":
+                    playerModel.rotationVelocity = 0;
+                    break;
+                case "ArrowRight" :
+                case "D":
+                    playerModel.rotationVelocity = 0;
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 }
 
