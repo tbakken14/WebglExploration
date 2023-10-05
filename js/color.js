230287174e6334813982ea9d8718e5a2e4200593
Thomas Bakken <tbakken14@gmail.com>
2023-10-04 21:01:22 -0700

    Add alternating colors

--- js/color.js
+++ js/color.js
@@ -1,7 +1,11 @@
 class Color {
     static rgb = [1, .5, 0,
-                  0, .5, 0,
-                  .5, .5, 1];
+                  0, .5, 2,
+                  .5, .7, 0 ];
+
+    static colorX = [0, 0, 0,
+                     0, 0, 1,
+                     0, 0, 0];
 
     static solidColor(r, g, b) {
         return [r, g, b,
@@ -9,12 +13,27 @@ class Color {
                 r, g, b]
     }
 
-    static buildColors(numTriangles, triColor) {
+    static buildColors(numTriangles, triColor, alternate = false) {
         let colors = [];
         for (let i = 0; i < numTriangles; i++) {
-            colors = colors.concat(triColor);
+            if (alternate && (i & 1)) {
+                colors = colors.concat(Color.triColorReverse(triColor));
+            }
+            else{
+                colors = colors.concat(triColor);
+            }
         }
         return colors;
+    }
+
+    static triColorReverse(triColor) {
+        return triColor.map((e, i, a) => {
+            const row = Math.floor(i / 3);
+            const col = i % 3;
+            const i_prime = 3 * (2 - row) + col;
+            //console.log(i_prime)
+            return a[i_prime];
+        })
     }
 }
 
