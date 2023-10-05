3655886669aeabed76c6f1dc1bb7a2d806f4205f
Thomas Bakken <tbakken14@gmail.com>
2023-10-05 02:26:50 -0700

    Update asteroid spawn to have random properties

--- js/spawn.js
+++ js/spawn.js
@@ -3,7 +3,7 @@ import Shape from "./shape.js";
 import Color from "./color.js";
 
 class Spawn {
-    static rate = 10000;
+    static rate = 5000;
     static lastSpawnTime = 0;
 
     static spawnHandler() {
@@ -14,10 +14,33 @@ class Spawn {
         }
     }
     static spawnAsteroid() {
+        Spawn.rate -= 100;
+        const location = Spawn.#getSpawnLocation();
+        const rotationVelocity = (Math.random() * 2 - 1) / 50;
+        const scale = (Math.random()) * 2 + 1;
+        const translationVelocity = [(Math.random() * 2 - 1), (Math.random() * 2 - 1)];
         const asteroid = new Model(Shape.CirclePie(30, 20), 
                         Color.buildColors(20, Color.asteroid, true), 
-                        0, [150, 100], [3, 3], 
-                        0, [.2, .3], [0, 0], true, 90, true);
+                        0, location, [scale, scale], 
+                        rotationVelocity, translationVelocity, [0, 0], true, 30 * scale, true);
+    }
+
+    static #getSpawnLocation() {
+        const width = 1000;
+        const height = 1000;
+        const perimeterPercent = Math.random();
+        if (perimeterPercent < .25) {
+            return [0, perimeterPercent * height];
+        }
+        else if (perimeterPercent < .5) {
+            return [(perimeterPercent - .25) * width, height];
+        }
+        else if (perimeterPercent < .75) {
+            return [width, (.75 - perimeterPercent) * height];
+        }
+        else {
+            return [(1 - perimeterPercent) * width, 0]
+        }
     }
 }
 
