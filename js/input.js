5cf2f19a0e943f595f3f37ede2d8c6e1c800c6a3
Thomas Bakken <tbakken14@gmail.com>
2023-11-18 23:03:21 -0800

    Add ammo depletion and reload

--- js/input.js
+++ js/input.js
@@ -1,19 +1,22 @@
 import Spawn from "./spawn.js";
+import Profile from "./profile.js";
 
 class Input {
+    static ammo = Profile.ammo;
+    static isReloading = false;
     static addKeyboardInputListeners() {
         document.addEventListener("keydown", (event) => {
             if (event.repeat) { return }
             switch (event.key) {
                 case "ArrowUp" :
                 case "w":
-                    Spawn.player.translationVelocity[0] += Math.cos(Spawn.player.rotation);
-                    Spawn.player.translationVelocity[1] += Math.sin(Spawn.player.rotation);
+                    Spawn.player.translationVelocity[0] += Profile.speed*Math.cos(Spawn.player.rotation);
+                    Spawn.player.translationVelocity[1] += Profile.speed*Math.sin(Spawn.player.rotation);
                     break;
                 case "ArrowDown" :
                 case "s":
-                    Spawn.player.translationVelocity[0] -= .5*Math.cos(Spawn.player.rotation);
-                    Spawn.player.translationVelocity[1] -= .5*Math.sin(Spawn.player.rotation);
+                    Spawn.player.translationVelocity[0] -= Profile.speed*.5*Math.cos(Spawn.player.rotation);
+                    Spawn.player.translationVelocity[1] -= Profile.speed*.5*Math.sin(Spawn.player.rotation);
                     break;
                 case "ArrowLeft" :
                 case "a":
@@ -24,7 +27,17 @@ class Input {
                     Spawn.player.rotationVelocity = -.03;
                     break;
                 case " " :
-                    Spawn.spawnProjectile();
+                    if (!Input.isReloading) {
+                        Spawn.spawnProjectile();
+                        Input.ammo--;
+                        if (Input.ammo == 0) {
+                            Input.isReloading = true;
+                            setTimeout(() => {
+                                Input.ammo = Profile.ammo;
+                                Input.isReloading = false;
+                            }, Profile.reload * 1000)
+                        }
+                    }
                     break;
                 default:
                     break;
