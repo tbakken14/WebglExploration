59c6c04f5c81edd6398f041f9bd53d1531812a3b
Thomas Bakken <tbakken14@gmail.com>
2023-12-01 21:13:47 -0800

    Update styles

--- js/GUI.js
+++ js/GUI.js
@@ -1,27 +1,63 @@
 import Game from "./game.js";
+import Profile from "./profile.js";
 
 class GUI {
-    static newGameButton = this.createNewGameButton();
-
-    static showButtons() {
-
+    constructor() {
+        this.newGameButton = this.createNewGameButton();
+        this.upgradeButtons = this.createUpgradeButtons();
     }
 
-    static hideButtons() {
-        
+
+    showElements(elements) {
+        elements.forEach((e) => {
+            e.style["display"] = "inline";
+        });
+    }
+    hideElements(elements) {
+        elements.forEach((e) => {
+            e.style["display"] = "none";
+        });
     }
 
-    static createNewGameButton() {
-        newGameButton = document.createElement("button");
+    addToDocument(elements) {
+        const body = document.getElementsByTagName("body")[0];
+        elements.forEach((e) => {
+            body.append(e);
+        });
+    }
+
+    createNewGameButton() {
+        let newGameButton = document.createElement("button");
         newGameButton.setAttribute("id", "newGame");
         newGameButton.innerHTML = "New Game";
         newGameButton.onclick = () => {
-            gameOverElement.remove();
-            newGameButton.remove(); 
-            upgradeAmmoButton.remove(); 
-            upgradeSpeedButton.remove(); 
+            //gameOverElement.remove();
+            this.hideElements(this.upgradeButtons);
+            this.hideElements([this.newGameButton]);
             Game.startGame();
         };
+        this.addToDocument([newGameButton]);
         return newGameButton;
     }
+
+    createUpgradeButton(className, text, onclick) {
+        let body = document.getElementsByTagName("body")[0];
+        let upgradeButton = document.createElement("button");
+        upgradeButton.setAttribute("class", className);
+        upgradeButton.innerHTML = text;
+        upgradeButton.onclick = onclick;
+        return upgradeButton;
+    }
+    
+    createUpgradeButtons() {
+        let upgradeButtons = [];
+        upgradeButtons.push(this.createUpgradeButton("Upgrade", "Ammo Upgrade", () => Profile.upgradeAmmo()));
+        upgradeButtons.push(this.createUpgradeButton("Upgrade", "Reload Upgrade", () => Profile.reloadUpgrade()));
+        upgradeButtons.push(this.createUpgradeButton("Upgrade", "Speed Upgrade", () => Profile.speedUpgrade()));
+        this.hideElements(upgradeButtons);
+        this.addToDocument(upgradeButtons);
+        return upgradeButtons;
+    }
 }
+
+export default GUI;
